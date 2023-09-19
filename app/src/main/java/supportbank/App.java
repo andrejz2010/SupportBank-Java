0c7b6a827496fad8f5ff42f5aeecd0292c0831aa
andrejz2010 <zacharovandrej1@gmail.com>
2023-09-19 17:08:07 +0100

    update 3

--- app/src/main/java/supportbank/App.java
+++ app/src/main/java/supportbank/App.java
@@ -4,6 +4,7 @@
 package supportbank;
 
 import java.io.*;
+import java.text.NumberFormat;
 import java.util.*;
 
 
@@ -71,33 +72,39 @@ public class App {
         // Creating an ArrayList of listOfNames
         ArrayList<String> listOfNames = new ArrayList<>(values);
 
+
+
+
+        HashMap<String, Account> accountsByNames = new HashMap<String, Account>();
+
+
+
+
         for (String n : listOfNames) {
             Account account = new Account(n);
-            System.out.println(account.name);
+            accountsByNames.put(n, account);
         }
 
 
-
-
-
+        System.out.println(accountsByNames);
 
 
 
         try {
             //parsing a CSV file into BufferedReader class constructor
             BufferedReader br = new BufferedReader(new FileReader("/home/andrej/IdeaProjects/SupportBank-Java/SupportBank-Java/Transactions2014.csv"));
+            skip=true;
             while ((line = br.readLine()) != null) {
                 if (skip) {
                     skip = false; // Skip only the first line
                     continue;
                 }
                 String[] dataInTheLine = line.split(splitBy);
-              //  System.out.println("[Date=" + dataInTheLine[0] + ", From=" + dataInTheLine[1] + ", To=" + dataInTheLine[2] + ", Narrative=" + dataInTheLine[3] + ", Amount= " + dataInTheLine[4] + "]");
+                // System.out.println("[Date=" + dataInTheLine[0] + ", From=" + dataInTheLine[1] + ", To=" + dataInTheLine[2] + ", Narrative=" + dataInTheLine[3] + ", Amount= " + dataInTheLine[4] + "]");
 
+accountsByNames.get(dataInTheLine[1]).moneyFromBank+=Double.parseDouble(dataInTheLine[4]);
+accountsByNames.get(dataInTheLine[2]).moneyFromBank-=Double.parseDouble(dataInTheLine[4]);
 
-                if(account.getName().equals(dataInTheLine[1])){
-                    System.out.println("Match!");
-                }
 
 
 
@@ -108,7 +115,13 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Result: ");
+        NumberFormat formatter=NumberFormat.getCurrencyInstance(Locale.UK);
+        for (String n : listOfNames) {
+            String currency=formatter.format(accountsByNames.get(n).moneyFromBank);
+            System.out.println(n+" should get "+ currency + " from the bank" );;
 
+        }
 
 
     }
